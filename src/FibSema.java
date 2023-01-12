7c3543f7402c02f58de0d289082a73faeb28d373
DaveP80 <davidpaq1@gmail.com>
2023-01-12 00:42:30 -0500

    sema

---
+++ src/FibSema.java
@@ -0,0 +1,89 @@
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.concurrent.Semaphore;
+
+public class FibSema {
+
+    public static void main(String[] args) {
+        Semaphore sem = new Semaphore(1);
+        //put the bigger number value in thread one
+        new Thread(new NumOneThread("A", sem, BigInteger.valueOf(400))).start();
+        //put smaller value in thread two
+        new Thread(new NumTwoThread("B", sem, BigInteger.valueOf(100))).start();
+    }
+}
+class FibMap {
+    static HashMap<BigInteger, BigInteger> mp = new HashMap<>();
+}
+
+class Functions {
+    static BigInteger doMath(BigInteger n) {
+
+        if (FibMap.mp.containsKey(n)) {
+            return FibMap.mp.get(n);
+        }
+        int cv = n.compareTo(BigInteger.valueOf(3));
+        if (cv < 0) {
+            return BigInteger.valueOf(1);
+        }
+        FibMap.mp.put(n, doMath(n.subtract(BigInteger.valueOf(2))).add(doMath(n.subtract(BigInteger.valueOf(1)))));
+        return FibMap.mp.get(n);
+    }
+}
+
+class NumOneThread implements Runnable {
+    String name;
+    Semaphore sem;
+    BigInteger bg;
+
+    public NumOneThread(String name, Semaphore sem, BigInteger bg) {
+        this.name = name;
+        this.sem = sem;
+        this.bg = bg;
+    }
+
+    @Override
+    public void run() {
+
+        System.out.println("Starting " + name);
+
+        try {
+            sem.acquire();
+
+            System.out.println(FibMap.mp.isEmpty());
+
+            System.out.println(Functions.doMath(bg));
+
+        } catch (InterruptedException exc) {
+            System.out.println(exc);
+        }
+        sem.release();
+    }
+}
+class NumTwoThread implements Runnable {
+    String name;
+    Semaphore sem;
+
+    BigInteger bg;
+
+    public NumTwoThread(String name, Semaphore sem, BigInteger bg) {
+        this.name = name;
+        this.sem = sem;
+        this.bg = bg;
+    }
+
+    @Override
+    public void run() {
+
+        System.out.println("Starting " + name);
+
+        try {
+            sem.acquire();
+            System.out.println(FibMap.mp.isEmpty());
+            System.out.println(Functions.doMath(bg));
+        } catch (InterruptedException exc) {
+            System.out.println(exc);
+        }
+        sem.release();
+    }
+}
